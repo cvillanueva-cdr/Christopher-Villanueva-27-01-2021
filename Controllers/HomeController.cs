b820b1c626cdd86d79190bd9bdd84f00a83ab0bf
cvillanueva <cvillanueva@novit.pe>
2021-01-27 17:37:17 -0500

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace AppEjemplo01
+{
+public class HomeController : AsyncController
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        } 
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DropDownListFor_MVC.MvcApplication" Language="C#" %>
